3fc8dfae78c065d4aa2122467b0785d5b6a9f992
vishal <praveenyoungind@gmail.com>
2016-07-30 01:20:47 +0530

    changes

--- users_eone/app/controllers/users_eone/home_controller.rb
+++ users_eone/app/controllers/users_eone/home_controller.rb
@@ -4,7 +4,7 @@ module UsersEone
   class HomeController < ApplicationController
 
     def index
-    	@users = User.page(params[:page]).per(30)
+    	@users = User.Order('id').page(params[:page]).per(30)
     end
   end
 end
